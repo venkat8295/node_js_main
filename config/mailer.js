eaffaa0f549843c349f4d71bfdc809acc9e15c56
venkat8295 <venkat8295@gmail.com>
2020-11-06 13:04:01 +0530

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MAILGUN_USER: 'postmaster@sandbox0ec57c53694543b7b558577233b2f2b2.mailgun.org',
-  MAILGUN_PASS: '0e08701cb699e2ef6954c8ee937063c8-ea44b6dc-f77e19cb'
+  MAILGUN_USER: 'postmaster@sample.mailgun.org',
+  MAILGUN_PASS: 'mailgun sample pass'
 };
